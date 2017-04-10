01bf0f0ef87e64895a49694ddc319e5b25d0ad22
Ruffeng <ruben.suet@gmail.com>
2017-04-10 11:04:48 +0200

    Added some setup to run up the server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const express = require('express'); // Equivalent to import
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+// Creating an express project
+const app = express();
+
 
 // App setup
 
@@ -12,3 +15,7 @@ const morgan = require('morgan');
 
 // Server Setup
 
+const port = process.env.PORT || 3090;
+const server = http.createServer(app); // Create an HTTP server
+server.listen(port);
+console.log('Server listening on:', port);
