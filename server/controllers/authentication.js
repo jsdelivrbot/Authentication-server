212651d15742d5811115f92aeff10cbeee356efa
Ruffeng <ruben.suet@gmail.com>
2017-04-10 15:15:14 +0200

    Checking if a user exists on the DB

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,19 @@
+const User = require ('../models/user');
 exports.signup = (req,res,next) => {
-  res.send({success: 'true'});
 
+  //res.send({success: 'true'});
+
+   // Everything it contains on the request
+   const email = req.body.email;
+   const password = req.body.password;
+  // See if a user with the given email exists
+   User.findOne({email: email}, (error,existingUser)=>{
+
+   });
+
+  // If a user with email does exist, return an error
+
+  // If a user with email does NOT exist , create and save user record
+
+  // Respond to request
 }
