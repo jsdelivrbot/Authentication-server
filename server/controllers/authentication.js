6150e16808c4d8b5da7c8d7e7d553a405002e782
Ruffeng <ruben.suet@gmail.com>
2017-04-10 15:25:44 +0200

    Now we're able to add users on the DB

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -8,12 +8,32 @@ exports.signup = (req,res,next) => {
    const password = req.body.password;
   // See if a user with the given email exists
    User.findOne({email: email}, (error,existingUser)=>{
+      if(error) {
+          return next(error);
+      }
+
+      // If a user with email does exist, return an error
+      if(existingUser){
+        return res.status(422).send({error: "email is in use"}); // Unprocessable entity
+      }
+
+      // If a user with email does NOT exist , create and save user record
+      // We created the model
+      const user = new User({
+        email: email,
+        password: password
+      });
+
+      // Time to save onto the DB
+      user.save((error)=>{
+          if(error) {return next(error);}
+          //Respond to equest indicating the user was created
+          res.json(user);
+      });
 
    });
 
-  // If a user with email does exist, return an error
 
-  // If a user with email does NOT exist , create and save user record
 
   // Respond to request
 }
