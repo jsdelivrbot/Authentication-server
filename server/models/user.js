8a6a1a21acba9125b7258664d52423a5580eac0f
Ruffeng <ruben.suet@gmail.com>
2017-04-10 12:02:25 +0200

    Created the user schema

--- server/models/user.js
+++ server/models/user.js
@@ -5,13 +5,16 @@ const Schema = mongoose.Schema;
 
 // Let's define our model
 const userSchema = new Schema({
-  email: String,
+  email: {type: String, unique: true, lowercase: true},
   password: String
 });
 
 
 // Lets' create our model class
-
+// It loads the model into a mongodb
+const ModelClass = mongoose.model('user', userSchema);
 
 
 // Lets' export the model
+
+module.exports = ModelClass;
