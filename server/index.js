fba966ae2b4f9722b24fd476a59b3521b8639719
Ruffeng <ruben.suet@gmail.com>
2017-04-10 10:56:21 +0200

    Added the dependencies of the server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+// Main starting point of the application
+// We well do it with ES5 Javascript instead
+
+const express = require('express'); // Equivalent to import
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// App setup
+
+
+
+// Server Setup
+
