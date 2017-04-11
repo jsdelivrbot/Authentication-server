c1de3b20f5080958cb1c6973fa0b394d96fceb9f
Ruffeng <ruben.suet@gmail.com>
2017-04-11 16:07:04 +0200

    Encrypting password for users

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt-nodejs');
 // Is the schema that we will define
 const Schema = mongoose.Schema;
 
@@ -10,6 +10,19 @@ const userSchema = new Schema({
   password: String
 });
 
+// On Save Hook, encrypt password
+userSchema.pre('save', (next)=>{
+  const user = this;
+  bcrypt.genSalt(10, (err,salt)=>{
+    if(err) {return next(err);}
+    bcrypt.hash(user.password, salt, null, (err,hash)=>{
+      if(err) {return next(err);}
+
+      user.password = hash;
+      next();
+    });
+  });
+});
 
 // Lets' create our model class
 // It loads the model into a mongodb
