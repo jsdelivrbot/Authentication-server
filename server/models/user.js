5cd8f336fa8a1c6c147b3294269ea49766bfe12d
Ruffeng <ruben.suet@gmail.com>
2017-04-11 16:33:13 +0200

    Added some comments to figure out what's going on there

--- server/models/user.js
+++ server/models/user.js
@@ -11,17 +11,27 @@ const userSchema = new Schema({
 });
 
 // On Save Hook, encrypt password
+// Before saving a model, run this function
 userSchema.pre('save', (next)=>{
+  // get access to the user model
   const user = this;
+
+  // Generate a salt, then run a callback
   bcrypt.genSalt(10, (err,salt)=>{
     if(err) {return next(err);}
+
+    // Hash our password using the salt
     bcrypt.hash(user.password, salt, null, (err,hash)=>{
       if(err) {return next(err);}
 
+      // Just overwrite plaun text password with encrypted password
       user.password = hash;
+      // just go ahead and continue with save
       next();
     });
+
   });
+
 });
 
 // Lets' create our model class
