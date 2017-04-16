f411eab8a3e355acf7f7e5b752ba6ffc8bfd9329
Ruffeng <ruben.suet@gmail.com>
2017-04-16 09:34:28 +0200

    creating then and catch

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,13 +5,19 @@ export function signinUser({email,password}){
     return function(dispatch){
       // submitting the request
       // {email: email, password: password}
-      axios.post(`${ROOT_URL}/signin`, {email,password});
-    // if request is good...
-    // -Update state to indicate user is auth
-    // save the JWT token
-    // redirect to the route '/feature'
+      axios.post(`${ROOT_URL}/signin`, {email,password})
+        .then(res=>{
+        // if request is good...
+        // -Update state to indicate user is auth
+        // save the JWT token
+        // redirect to the route '/feature'
 
-    // If request is bad...
-    // -Show an error to the user
+        })
+
+        .catch(()=>{
+        // If request is bad...
+        // -Show an error to the user
+
+        });
     }
 }
