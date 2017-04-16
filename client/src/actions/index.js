bf42d001f36e82066bf25b420a75e25912abe9cf
Ruffeng <ruben.suet@gmail.com>
2017-04-16 09:40:32 +0200

    Now user can auth, time to prepare the redirection page

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,17 +1,20 @@
 import axios from 'axios';
+import {browserHistory} from 'react-router';
 const ROOT_URL = 'http://localhost:3090';
-
 export function signinUser({email,password}){
     return function(dispatch){
       // submitting the request
       // {email: email, password: password}
       axios.post(`${ROOT_URL}/signin`, {email,password})
+        // The then will entry just in case that the user exists, because we prepared the server
+        // to respond with a fail
         .then(res=>{
+
         // if request is good...
         // -Update state to indicate user is auth
         // save the JWT token
-        // redirect to the route '/feature'
-
+        // redirect to the route '/feature' -> Thanks to the browserHistory
+          browserHistory.push('/feature');
         })
 
         .catch(()=>{
