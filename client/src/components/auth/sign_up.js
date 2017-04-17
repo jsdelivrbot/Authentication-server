33217da608b284b1e26dd22bd4adb41306fa0463
Ruffeng <ruben.suet@gmail.com>
2017-04-17 12:29:07 +0200

    Scaffolded the signup component and route

---
+++ client/src/components/auth/sign_up.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import {reduxForm} from 'redux-form';
+import {connect} from 'react-redux';
+import * as actions from '../../actions';
+
+
+class Signup extends Component{
+  render(){
+    return(
+    <div className="">
+      Sign Up Form
+    </div>
+    );
+  }
+}
+
+export default Signup;
--- client/src/routes.js
+++ client/src/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute} from 'react-router';
 import App from './components/app';
 import SignIn from './components/auth/sign_in';
 import Signout from './components/auth/sign_out';
+import Signup from './components/auth/sign_up';
 
 export default(
   <Route path="/" component={App}>
       <Route path="signin" component={SignIn}></Route>
       <Route path="signout" component={Signout}></Route>
+      <Route path="signup" component={Signup}></Route>
   </Route>
 );
