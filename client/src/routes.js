07105da33a9b17d2a600685df78c761bf41420bb
Ruffeng <ruben.suet@gmail.com>
2017-04-17 12:06:31 +0200

    Added the logic to sign out for the user. Time to do the action creator

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route, IndexRoute} from 'react-router';
 import App from './components/app';
 import SignIn from './components/auth/sign_in';
+import Signout from './components/auth/sign_out';
 
 export default(
   <Route path="/" component={App}>
       <Route path="signin" component={SignIn}></Route>
+      <Route path="signout" cimponent={Signout}></Route>
   </Route>
 );
