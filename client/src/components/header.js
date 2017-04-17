ae0474dd8ddd6babb4d16b9588bbb0f397cb5983
Ruffeng <ruben.suet@gmail.com>
2017-04-17 10:38:20 +0200

    Added some logic to the header. Now if you're not signed up , you see the form to go there. Otherwise, you can sign out

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,13 +1,33 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router';
 class Header extends Component{
-
+  renderLinks(){
+    if(this.props.auth){
+      return(
+        <li className="nav-item">
+           <Link className="nav-link" to="/signout"> Signout </Link>
+        </li>
+      );
+    }
+    else{
+      return [
+        <li className="nav-item" key={1}>
+           <Link className="nav-link" to="/signin"> Sign In  </Link>
+        </li>,
+        <li className="nav-item" key={2}>
+           <Link className="nav-link"  to="/signup"> Sign Up </Link>
+        </li>
+      ];
+    }
+  }
   render(){
+
     return(
       <nav className="navbar navbar-light">
+        <Link to="/" className="navbar-brand"> Redux Auth</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">
-              Sign in
-          </li>
+          {this.renderLinks()}
         </ul>
       </nav>
     );
@@ -15,4 +35,7 @@ class Header extends Component{
 
 }
 
-export default Header;
+function mapStateToProps(state){
+  return { auth: state.auth.authenticated}
+}
+export default connect (mapStateToProps)(Header);
