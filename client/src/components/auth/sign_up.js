8eded6a406c6767c8dd38bc617e950fe297d7148
Ruffeng <ruben.suet@gmail.com>
2017-04-17 12:56:00 +0200

    Created the form to sign up. Time to do the validation

--- client/src/components/auth/sign_up.js
+++ client/src/components/auth/sign_up.js
@@ -6,12 +6,30 @@ import * as actions from '../../actions';
 
 class Signup extends Component{
   render(){
+    const { handleSubmit ,fields: {email,password,passwordConfirm}} = this.props;
     return(
-    <div className="">
-      Sign Up Form
-    </div>
+      <form action="">
+        <fieldset className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input  className="form-control" {...email}/>
+        </fieldset>
+
+        <fieldset className="form-group">
+          <label htmlFor="">Password:</label>
+          <input type="password" className="form-control" {...password}/>
+        </fieldset>
+
+        <fieldset className="form-group">
+          <label htmlFor="">Confirm Password:</label>
+          <input type="password" className="form-control" {...passwordConfirm}/>
+        </fieldset>
+        <button type="submit" className="btn btn-primary">Sign up!</button>
+      </form>
     );
   }
 }
 
-export default Signup;
+export default reduxForm({
+  form: 'signup',
+  fields: ['email','password','passwordConfirm']
+})(Signup);
