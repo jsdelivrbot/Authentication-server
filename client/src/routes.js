d0761692aa93be05093beb143a7b292366e6353a
Ruffeng <ruben.suet@gmail.com>
2017-04-18 11:03:13 +0200

    Scaffolded feature

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,11 +4,13 @@ import App from './components/app';
 import SignIn from './components/auth/sign_in';
 import Signout from './components/auth/sign_out';
 import Signup from './components/auth/sign_up';
+import Feature from './components/feature';
 
 export default(
   <Route path="/" component={App}>
       <Route path="signin" component={SignIn}></Route>
       <Route path="signout" component={Signout}></Route>
       <Route path="signup" component={Signup}></Route>
+      <Route path="feature" component={Feature}></Route>
   </Route>
 );
